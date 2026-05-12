e7f6d4406478d3fd137c115916c0594f3b4d54f7
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-12 21:51:09 +0900

    template : HTTPRequest/Client를 이용한 다른 API 요청/응답 방식으로 템플릿 정상 동작 테스트

--- sy-main/spring/hellospring/src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ sy-main/spring/hellospring/src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -3,16 +3,16 @@ package tobyspring.hellospring.exrate;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
-import tobyspring.hellospring.api.ApiExecutor;
-import tobyspring.hellospring.api.ApiTemplate;
-import tobyspring.hellospring.api.ExRateExtractor;
-import tobyspring.hellospring.api.SimpleApiExecutor;
+import tobyspring.hellospring.api.*;
 import tobyspring.hellospring.payment.ExRateProviderInterface;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Component
 public class WebApiExRateProvider implements ExRateProviderInterface {
@@ -23,7 +23,7 @@ public class WebApiExRateProvider implements ExRateProviderInterface {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
         // 재사용 가능성이 없는 경우 해당 객체는 람다로 표현하는 것도 낫뱃?
-        return apiTemplate.getExRate(url, new SimpleApiExecutor(), response -> {
+        return apiTemplate.getExRate(url, new HttpClientApiExercutor(), response -> {
             ObjectMapper mapper = new ObjectMapper();
             ExRateData data = mapper.readValue(response, ExRateData.class);
             return data.rates().get("KRW");
