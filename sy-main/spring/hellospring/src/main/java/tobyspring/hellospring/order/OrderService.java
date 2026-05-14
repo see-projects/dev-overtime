12bfa610dfbe527c562b2a46ae29edad79b99a1c
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-14 21:48:15 +0900

    service : OrderService 간단 구현

---
+++ sy-main/spring/hellospring/src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -0,0 +1,30 @@
+package tobyspring.hellospring.order;
+
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
+import tobyspring.hellospring.data.OrderRepository;
+
+import java.math.BigDecimal;
+
+@Service
+public class OrderService {
+    private OrderRepository orderRepository;
+    private JpaTransactionManager transactionManager;
+
+
+    public OrderService(OrderRepository orderRepository, JpaTransactionManager  transactionManager) {
+        this.orderRepository = orderRepository;
+        this.transactionManager = transactionManager;
+    }
+
+    public Order createOrder(String no, BigDecimal total) {
+        Order order = new Order(no, total);
+
+        return new TransactionTemplate(transactionManager).execute(status -> {
+
+            this.orderRepository.save(order);
+            return order;
+        });
+    }
+}
