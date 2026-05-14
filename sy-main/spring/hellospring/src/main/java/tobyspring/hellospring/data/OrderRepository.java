9ed0fbb15bf4b78fef9aa83a86b56973aa98e55a
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-14 21:38:39 +0900

    @exception&DB : 스프링 기술을 활용한 repository, client, configuration 구성

--- sy-main/spring/hellospring/src/main/java/tobyspring/hellospring/data/OrderRepository.java
+++ sy-main/spring/hellospring/src/main/java/tobyspring/hellospring/data/OrderRepository.java
@@ -3,39 +3,16 @@ package tobyspring.hellospring.data;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceContext;
 import tobyspring.hellospring.order.Order;
 
 import java.math.BigDecimal;
 
 public class OrderRepository {
-    private EntityManagerFactory emf;
-
-    public OrderRepository(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public void save(Order order) {
-        // em
-        EntityManager em = emf.createEntityManager();
-
-        // transaction
-        // transaction-start
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-
-        try {
-        //em.persist
-        em.persist(order);
-        em.flush();
-        System.out.println(order);
-        transaction.commit();
-
-        } catch (RuntimeException e) {
-            if(transaction.isActive()) transaction.rollback();
-            throw e;
-        } finally {
-            // transaction-close
-            if (em.isOpen()) em.close();
-        }
+        entityManager.persist(order);
     }
 }
