2619547741dfbe2b595f2ff6391b644d6e012d20
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-18 22:56:57 +0900

    springboot(servlet container) : 프론트 컨트롤러 설정해보기

--- sy-main/spring/helloboot/src/main/java/com/example/demo/DemoApplication.java
+++ sy-main/spring/helloboot/src/main/java/com/example/demo/DemoApplication.java
@@ -12,6 +12,8 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.io.IOException;
@@ -31,9 +33,11 @@ public class DemoApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능들을 여기에서 처리한다는 방식
+                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 
                     String name = req.getParameter("name");
 
@@ -41,8 +45,15 @@ public class DemoApplication {
                     resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
                     resp.setStatus(200);
                     resp.getWriter().println("<h1>Hello " + name + "World!</h1>");
+                    }
+                    else if(req.getRequestURI().equals("/users")) {
+                        //
+                    }
+                    else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");
 
         });
         webServer.start();
