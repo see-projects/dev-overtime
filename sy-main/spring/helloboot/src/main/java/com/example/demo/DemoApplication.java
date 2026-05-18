1175dc81a7535bf2907df110e8df047858d70b61
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-18 22:40:43 +0900

    springboot(servlet container) : 실제 서블릿 연결해보기

--- sy-main/spring/helloboot/src/main/java/com/example/demo/DemoApplication.java
+++ sy-main/spring/helloboot/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,18 @@
 package com.example.demo;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import java.io.IOException;
 
 //@SpringBootApplication
 //public class DemoApplication {
@@ -20,7 +28,17 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setContentType("text/html;charset=utf-8");
+                    resp.setStatus(200);
+                    resp.getWriter().println("<h1>Hello World!</h1>");
+                }
+            }).addMapping("/hello");
+
+        });
         webServer.start();
     }
 
