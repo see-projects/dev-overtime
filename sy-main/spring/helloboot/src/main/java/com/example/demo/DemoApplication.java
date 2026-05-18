9f50abe9e808a97f79157f4dd10afb70267bb248
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-18 22:25:44 +0900

    springboot(servlet container) : 독립실행형 애플리케이션 이전 서블릿 컨테이너 설정 방식 구현해보기

--- sy-main/spring/helloboot/src/main/java/com/example/demo/DemoApplication.java
+++ sy-main/spring/helloboot/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
+//@SpringBootApplication
+//public class DemoApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(DemoApplication.class, args);
+//    }
+//
+//}
+
+
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
