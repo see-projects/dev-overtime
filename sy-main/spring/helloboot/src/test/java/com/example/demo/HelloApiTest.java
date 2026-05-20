65e6d357e68f6ab1653e835a71df11567e4fc3db
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-20 22:38:25 +0900

    springboot(test) : 테스트 코드 간단작성

---
+++ sy-main/spring/helloboot/src/test/java/com/example/demo/HelloApiTest.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate restTemplate = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                restTemplate.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status code 200
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header(content-type) text/plain
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body Hello Spring
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+}
