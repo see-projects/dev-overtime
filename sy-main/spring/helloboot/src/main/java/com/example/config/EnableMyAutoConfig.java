6f75d9b898fd930e4b2d4414879aff2fb463fee2
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-21 22:33:22 +0900

    springboot(importselector) : importSelector로 동적으로 자동 구성 정보 설정

--- sy-main/spring/helloboot/src/main/java/com/example/config/EnableMyAutoConfig.java
+++ sy-main/spring/helloboot/src/main/java/com/example/config/EnableMyAutoConfig.java
@@ -11,6 +11,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({DispatcherServiceConfig.class, TomcatWebServerConfig.class})
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfig {
 }
