4bcb5588123c444f561e339dba79620be7d0cbda
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-21 22:48:11 +0900

    springboot(importselector) : 위치 자동 설정까지 구현

--- sy-main/spring/helloboot/src/main/java/com/example/config/MyAutoConfigImportSelector.java
+++ sy-main/spring/helloboot/src/main/java/com/example/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,28 @@
 package com.example.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "com.example.config.autoconfig.DispatcherServiceConfig",
-                "com.example.config.autoconfig.TomcatWebServerConfig"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(candidate ->
+            autoConfigs.add(candidate)
+        );
+        return autoConfigs.toArray(new String[0]);
+
     }
 }
