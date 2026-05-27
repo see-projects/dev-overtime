7317cb86c2c0a65f48eadc06ab2dfc503a9a247f
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-27 21:50:01 +0900

    springboot(Value) : @Value로 프로퍼티값 가져와 등록

--- sy-main/spring/helloboot/src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ sy-main/spring/helloboot/src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -16,11 +17,15 @@ import org.springframework.util.ClassUtils;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("TomcatWebServerConfig")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletContainer(Environment environment) {
+    public ServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        serverFactory.setContextPath(environment.getProperty("contextPath"));
+
+        serverFactory.setContextPath(contextPath);
         return serverFactory;
 
     }
