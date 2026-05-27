9efb7f970aebb8808b6b5d10041cdae26599931b
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-27 17:04:48 +0900

    springboot(customWebServerFactory) : 애플리케이션에서 직접 구성 정보를 설정하게 되고, 자동 구성 정보 설정을 후순위로 밀기

--- sy-main/spring/helloboot/src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
+++ sy-main/spring/helloboot/src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -16,6 +17,7 @@ import org.springframework.util.ClassUtils;
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("JettyWebServerConfig")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletContainer() {
         return new JettyServletWebServerFactory();
     }
--- sy-main/spring/helloboot/src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ sy-main/spring/helloboot/src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.util.ClassUtils;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("TomcatWebServerConfig")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletContainer() {
         return new TomcatServletWebServerFactory();
     }
