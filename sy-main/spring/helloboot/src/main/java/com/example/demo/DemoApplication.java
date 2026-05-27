428fd5bd06ac4f9d51e412d4c3992afb8dda4d3c
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-27 21:36:24 +0900

    springboot(applicationRunner) : 환경변수가 설정된 환경의 우선순위에 따른 같은 프로퍼티 변수명으로 다른 값 출력 확인

--- sy-main/spring/helloboot/src/main/java/com/example/demo/DemoApplication.java
+++ sy-main/spring/helloboot/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
 import com.example.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 //@SpringBootApplication
 //public class DemoApplication {
@@ -19,6 +22,14 @@ public class DemoApplication {
 //
 //        MySpringApplication.run(HelloConfig.class, args);
 //    }
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment environment) {
+        return args -> {
+            String name = environment.getProperty("my.name");
+            System.out.println("name" + name);
+        };
+    }
 
     // 기본 SpringApplication.run start 코드랑 동일한 형태가 됨
     public static void main(String[] args) {
