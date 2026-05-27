717bfabac681a73e6de9b0d28b67d88b21e6be6d
okandycho-bit <okandycho@rockwonitglobal.com>
2026-05-27 22:01:01 +0900

    springboot(Binder) : Environment로 귀찮게 가져오는 것보다 Binder로 프토퍼티 집합 클래스를 가져와 자동 설정되게끔 구성

--- sy-main/spring/helloboot/src/main/java/com/example/config/autoconfig/ServerPropertiesConfig.java
+++ sy-main/spring/helloboot/src/main/java/com/example/config/autoconfig/ServerPropertiesConfig.java
@@ -1,6 +1,7 @@
 package com.example.config.autoconfig;
 
 import com.example.config.MyAutoConfiguration;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
@@ -9,11 +10,6 @@ public class ServerPropertiesConfig {
 
     @Bean
     public ServerProperties serverProperties(Environment environment) {
-        ServerProperties properties = new ServerProperties();
-
-        properties.setContextPath(environment.getProperty("contextPath"));
-        properties.setPort(Integer.parseInt(environment.getProperty("port")));
-
-        return properties;
+        return Binder.get(environment).bind("", ServerProperties.class).get();
     }
 }
