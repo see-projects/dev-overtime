229173f726465be98033609a2617e33954d541b7
layout-SY <boycho2000@naver.com>
2026-07-13 11:44:22 +0900

    feat(Email) : Email Value 타입 구현

---
+++ sy-main/spring/splearn/src/main/java/tobyspring/splearn/application/required/EmailSender.java
@@ -0,0 +1,7 @@
+package tobyspring.splearn.application.required;
+
+import tobyspring.splearn.domain.Email;
+
+public interface EmailSender {
+    void send(Email email, String subject, String body);
+}
